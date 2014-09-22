c8d3b69be399fb56428faa63c5bd36199ea03572
mort <martin@mort.coffee>
2014-09-23 01:21:30 +0200

    prettier default theme, and fixed browser incompatibility issue

--- theme/default/js/script.js
+++ theme/default/js/script.js
@@ -11,5 +11,11 @@ var posts = $(".entry .content");
 posts.forEach(function(element)
 {
 	if (element.offsetHeight < 300 || posts.length <= 1)
+	{
 		element.parentNode.className += " expanded";
+
+		//hack for safari
+		element.style.minHeight = element.offsetHeight+"px";
+		console.log(element.style.minHeight);
+	}
 });
